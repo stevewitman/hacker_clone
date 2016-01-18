ebeda4eb0180f51376f9cd279ac64b5b0f7d8160
Steve Witman <stevewitman@gmail.com>
2016-01-17 21:35:48 -0700

    added form to get user input

--- app.js
+++ app.js
@@ -9,5 +9,10 @@ angular.module('hackerClone', [])
 			{title: 'post 4', upvotes: 9},
 			{title: 'post 5', upvotes: 4},
 		];
+		$scope.addPost = function() {
+			if(!$scope.title || $scope.title === '') {return;}
+			$scope.posts.push({title: $scope.title, upvotes: 0});
+			$scope.title = '';
+		};
 	}
 ]);
